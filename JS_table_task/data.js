92ad224340c9abee66b78612898efb3f73af5e8a
Pradeep9099 <pradeepkumarpudari@gmail.com>
2022-11-19 10:00:28 +0530

    upload

---
+++ JS_table_task/data.js
@@ -0,0 +1,92 @@
+var details = [
+    {
+        "fname": "Thomas",
+        "lname": "Swanson",
+        "tel": "(265)584-9018",
+        "address": "7765 Elit Ln",
+        "city": "Middletown",
+        "state": "AR",
+        "zip": 43133
+    },
+    {
+        "fname": "Dennis",
+        "lname": "Diaz",
+        "tel": "(623)402-1443",
+        "address": "5252 Tempor Rd",
+        "city": "Lenoir",
+        "state": "NC",
+        "zip": 84182
+    },
+    {
+        "fname": "Sudershan",
+        "lname": "Melendez",
+        "tel": "(353)847-2702",
+        "address": "6357 Libero Ct",
+        "city": "Gig Harbor",
+        "state": "MD",
+        "zip": 96560
+    },
+    {
+        "fname": "Girish",
+        "lname": "Pickett",
+        "tel": "(111)084-9141",
+        "address": "8168 Amet Dr",
+        "city": "Mineral Bluff",
+        "state": "AK",
+        "zip": 83453
+    },
+    {
+        "fname": "Vaibhavi",
+        "lname": "Neilson",
+        "tel": "(404)698-6229",
+        "address": "6949 Magna Ln",
+        "city": "Lockport",
+        "state": "MN",
+        "zip": 32614
+    },
+    {
+        "fname": "Kurt",
+        "lname": "Jezak",
+        "tel": "(924)663-0917",
+        "address": "1637 Nunc Rd",
+        "city": "Johnsburg",
+        "state": "WY",
+        "zip": 75769
+    },
+    {
+        "fname": "Hsiao-Shan",
+        "lname": "Bliss",
+        "tel": "(388)814-8089",
+        "address": "9904 Consequat Ln",
+        "city": "Seven Mile",
+        "state": "OK",
+        "zip": 38984
+    },
+    {
+        "fname": "Jeri",
+        "lname": "Lamsargis",
+        "tel": "(820)169-5961",
+        "address": "4279 Quis Ave",
+        "city": "Kingsville",
+        "state": "RI",
+        "zip": 77168
+    },
+    {
+        "fname": "Jephter",
+        "lname": "Gaby",
+        "tel": "(373)514-9850",
+        "address": "5943 Tincidunt Ave",
+        "city": "Grand Forks",
+        "state": "OH",
+        "zip": 63912
+    },
+    {
+        "fname": "LaDagea",
+        "lname": "Bender",
+        "tel": "(740)456-2860",
+        "address": "2023 Tempor Rd",
+        "city": "The Dalles",
+        "state": "VA",
+        "zip": 91716
+    }
+]
