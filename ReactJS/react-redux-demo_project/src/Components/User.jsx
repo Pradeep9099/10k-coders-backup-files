bc9234d30790d6d9c1d0fb650ec6ec8c70d90dab
Pradeep9099 <pradeepkumarpudari@gmail.com>
2023-01-04 11:38:23 +0530

    ok

--- ReactJS/react-redux-demo_project/src/Components/User.jsx
+++ ReactJS/react-redux-demo_project/src/Components/User.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { addUserAction, deleteUserAction } from "./Actions";
+import { addUserAction, deleteUserAction } from "../Store/Actions";
 // import { addUserAction, deleteUserAction } from "";
 
 class User extends Component {
@@ -8,13 +8,13 @@ class User extends Component {
         console.log(this.props)
         return (
             <div>
-                <button onClick={this.props.addUser}>Add User</button>
+                {/* <button onClick={this.props.addUser}>Add User</button>
                 <button onClick={this.props.deleteUser}>Delete User</button>
                 <ul>
                     {this.props.allUsers && this.props.allUsers.map((user,i)=> <li key={i} onClick={()=>{
                        this.props.deleteUser(user)
                     }}>{user}</li>)}
-                </ul>
+                </ul> */}
             </div>
         )
     }
